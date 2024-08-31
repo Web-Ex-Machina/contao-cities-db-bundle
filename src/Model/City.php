7f44ec184a823247202709d2f8eafe16656e6f6e
LupusVII <jln.thirion@gmail.com>
2024-08-31 11:15:37 +0200

    Adjust Model

--- src/Model/City.php
+++ src/Model/City.php
@@ -2,20 +2,9 @@
 
 declare(strict_types=1);
 
-/**
- * Contao Offers for Contao Open Source CMS
- * Copyright (c) 2018-2024 Web ex Machina.
- *
- * @category ContaoBundle
- *
- * @author   Web ex Machina <contact@webexmachina.fr>
- *
- * @see     https://github.com/Web-Ex-Machina/contao-offers/
- */
-
 namespace WEM\CitiesDbBundle\Model;
 
-use Contao\Model\Registry;
+use Contao\Model;
 
 /**
  * Reads and writes items.
